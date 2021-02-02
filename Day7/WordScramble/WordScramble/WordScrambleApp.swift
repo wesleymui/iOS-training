61a93aa8e03fb24446d75f0a890c0e1405b2f51d
wesleymui <68392398+wesleymui@users.noreply.github.com>
2021-02-02 17:18:56 -0500

    WordScramble - Project 5

---
+++ Day7/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Wesley Mui on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
