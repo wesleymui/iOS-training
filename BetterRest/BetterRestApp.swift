bc1f478254a4c44498e2e27671279bdd69695a54
wesleymui <68392398+wesleymui@users.noreply.github.com>
2021-02-02 01:06:26 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Wesley Mui on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
