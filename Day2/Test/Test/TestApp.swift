8b46eb64eeaa18217ef489d9a8ec971a77602471
wesleymui <68392398+wesleymui@users.noreply.github.com>
2021-02-02 16:01:23 -0500

    Test Files

---
+++ Day2/Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Wesley Mui on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
